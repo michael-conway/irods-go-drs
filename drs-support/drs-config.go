2cd9d98e9901d61335599ca11c0c498eb688423b
Mike Conway <Michael.c.conway@gmail.com>
2025-04-07 06:48:17 -0400

    adding tests/config

--- drs-support/drs-config.go
+++ drs-support/drs-config.go
@@ -13,17 +13,45 @@ type DrsConfig struct {
 	DrsLogLevel   string //info, debug
 }
 
+const DefaultConfigName = "drs-config"
+const DefaultConfigType = "yaml"
+
+// Read the configuration for DRS behaviors in irods
+// can take a number of paths that will be prefixed in the searh path, or defaults
+// may be accepted, blank params for name and type defaut to irods-drs.yaml
 func ReadDrsConfig(configName string, configType string, configPaths []string) (*DrsConfig, error) {
-	viper.SetConfigName("config")         // name of config file (without extension)
-	viper.SetConfigType("yaml")           // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("/etc/appname/")  // path to look for the config file in
-	viper.AddConfigPath("$HOME/.appname") // call multiple times to add many search paths
-	viper.AddConfigPath(".")              // optionally look for config in the working directory
-	err := viper.ReadInConfig()           // Find and read the config file
-	if err != nil {                       // Handle errors reading the config file
-		panic(fmt.Errorf("fatal error config file: %w", err))
+
+	if configName == "" {
+		viper.SetConfigName(DefaultConfigName) // name of config file (without extension)
+	} else {
+		viper.SetConfigName(configName)
 	}
 
+	if configType == "" {
+		viper.SetConfigType(DefaultConfigType) // REQUIRED if the config file does not have the extension in the name
+	} else {
+		viper.SetConfigType(configType)
+	}
+	
+	for _, path := range configPaths {
+		viper.AddConfigPath(path)
+	}
+
+	viper.AddConfigPath("/etc/irods-ext/") // path to look for the config file in
+	viper.AddConfigPath("$HOME/.appname")  // call multiple times to add many search paths
+	viper.AddConfigPath(".")               // optionally look for config in the working directory
+	err := viper.ReadInConfig()            // Find and read the config file
+	if err != nil {                        // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+	var C DrsConfig
+
+	err = viper.Unmarshal(&C)
+	if err != nil {
+		panic(fmt.Errorf("unable to decode into struct, %v", err))
+	}
+
+	return &C, nil
 }
 
 func InitializeLogging(drsConfig *DrsConfig) {
---
+++ test/drs_config_test.go
@@ -0,0 +1,14 @@
+package test
+
+import (
+	"github.com/cyverse/go-irodsclient/irods/connection"
+	"github.com/cyverse/go-irodsclient/irods/fs"
+	"github.com/michael-conway/irods-go-drs/drs-support"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func ReadDrsConfig(t *testing.T) {
+	actual, err = ReadDrsConfig()
+}
