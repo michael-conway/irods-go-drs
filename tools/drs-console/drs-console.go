4080d7cefa1b4a5e4e16da53251049dd8acbd1f3
conwaymc <mike.conway@nih.gov>
2025-05-07 15:59:20 -0400

    incremental console buildout

--- tools/drs-console/drs-console.go
+++ tools/drs-console/drs-console.go
@@ -2,10 +2,36 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 
 	"github.com/urfave/cli/v3"
 )
+
+func init() {
+	cli.RootCommandHelpTemplate += "\nCUSTOMIZED: you bet ur muffins\n"
+	cli.CommandHelpTemplate += "\nYMMV\n"
+	cli.SubcommandHelpTemplate += "\nor something\n"
+
+	cli.HelpFlag = &cli.BoolFlag{Name: "halp"}
+	cli.VersionFlag = &cli.BoolFlag{Name: "print-version", Aliases: []string{"V"}}
+
+	cli.HelpPrinter = func(w io.Writer, templ string, data interface{}) {
+		fmt.Fprintf(w, "best of luck to you\n")
+	}
+	cli.VersionPrinter = func(cmd *cli.Command) {
+		fmt.Fprintf(cmd.Root().Writer, "version=%s\n", cmd.Root().Version)
+	}
+	cli.OsExiter = func(cmd int) {
+		fmt.Fprintf(cli.ErrWriter, "refusing to exit %d\n", cmd)
+	}
+	cli.ErrWriter = ioutil.Discard
+	cli.FlagStringer = func(fl cli.Flag) string {
+		return fmt.Sprintf("\t\t%s", fl.Names()[0])
+	}
+}
 
 func main() {
 	(&cli.Command{}).Run(context.Background(), os.Args)
