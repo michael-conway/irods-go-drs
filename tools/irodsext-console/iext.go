f85c48a79d66854e47c268bc413dc77a5c08f5cb
conwaymc <mike.conway@nih.gov>
2025-06-25 08:28:20 -0400

    working on iinit

--- tools/irodsext-console/iext.go
+++ tools/irodsext-console/iext.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/cyverse/go-irodsclient/irods/types"
 	"io"
 	"log/slog"
 	"os"
@@ -72,8 +73,9 @@ func main() {
 	var auth_type string
 	var zone string
 	var host string
-	var port string
+	var port int
 	var user string
+	var password string
 
 	cmd := &cli.Command{
 		Commands: []*cli.Command{
@@ -99,9 +101,9 @@ func main() {
 						Usage:       "host",
 						Destination: &host,
 					},
-					&cli.StringFlag{
+					&cli.IntFlag{
 						Name:        "o",
-						Value:       "1247",
+						Value:       1247,
 						Usage:       "port",
 						Destination: &port,
 					},
@@ -112,51 +114,23 @@ func main() {
 						Destination: &user,
 					},
 					&cli.StringFlag{
-						Name:  "p",
-						Value: "",
-						Usage: "password",
+						Name:        "p",
+						Value:       "",
+						Usage:       "password",
+						Destination: &password,
 					},
 				},
 				Action: func(ctx context.Context, cmd *cli.Command) error {
-					fmt.Println("added task: ", cmd.Args().First())
-					for i := 0; i < len(cmd.Flags); i++ {
-						flag := cmd.Flags[i]
-						logger.Info("name", flag.Names()[0], "value", flag.Get())
-
+					irodsAccount := types.IRODSAccount{
+						AuthenticationScheme: types.GetAuthScheme(auth_type),
+						Host:                 host,
+						Port:                 port,
+						ClientUser:           user,
+						ClientZone:           zone,
+						Password:             password,
 					}
+					logger.Info("iinit with irodsAccount:", irodsAccount)
 					return nil
-				},
-			},
-			{
-				Name:    "complete",
-				Aliases: []string{"c"},
-				Usage:   "complete a task on the list",
-				Action: func(ctx context.Context, cmd *cli.Command) error {
-					fmt.Println("completed task: ", cmd.Args().First())
-					return nil
-				},
-			},
-			{
-				Name:    "template",
-				Aliases: []string{"t"},
-				Usage:   "options for task templates",
-				Commands: []*cli.Command{
-					{
-						Name:  "add",
-						Usage: "add a new template",
-						Action: func(ctx context.Context, cmd *cli.Command) error {
-							fmt.Println("new task template: ", cmd.Args().First())
-							return nil
-						},
-					},
-					{
-						Name:  "remove",
-						Usage: "remove an existing template",
-						Action: func(ctx context.Context, cmd *cli.Command) error {
-							fmt.Println("removed task template: ", cmd.Args().First())
-							return nil
-						},
-					},
 				},
 			},
 		},
